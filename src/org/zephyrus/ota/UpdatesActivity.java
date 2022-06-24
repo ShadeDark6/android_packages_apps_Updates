SunOS-Project/android_packages_apps_Updater

Updates: Implement local update
* Based on https://github.com/crdroidandroid/android_packages_apps_Updater/commit/ccb64ad345e219ae82c75e60c634ded8161475b1.

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

src/org/nameless/updates/UpdatesActivity.java
@@ -50,12 +50,14 @@ import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
+import org.nameless.updates.ExtrasFragment.UpdateListener;
 import org.nameless.updates.controller.ABUpdateInstaller;
 import org.nameless.updates.controller.UpdaterController;
 import org.nameless.updates.controller.UpdaterService;
 import org.nameless.updates.download.DownloadClient;
 import org.nameless.updates.misc.Constants;
 import org.nameless.updates.misc.Utils;
+import org.nameless.updates.model.Update;
 import org.nameless.updates.model.UpdateInfo;
 import org.nameless.updates.model.UpdateStatus;
 
@@ -65,7 +67,7 @@ import java.util.UUID;
 
 import static org.nameless.updates.model.UpdateStatus.UNKNOWN;
 
-public class UpdatesActivity extends UpdatesListActivity {
+public class UpdatesActivity extends UpdatesListActivity implements UpdateListener {
 
     private static final String TAG = "UpdatesActivity";
 
@@ -307,6 +309,25 @@ public class UpdatesActivity extends UpdatesListActivity {
         return true;
     }
 
+    @Override
+    public void addedUpdate(Update update) {
+        UpdaterController controller = mUpdaterService.getUpdaterController();
+        Utils.setPersistentStatus(this, UpdateStatus.Persistent.VERIFIED);
+        controller.addUpdate(update);
+        getUpdatesList();
+        Utils.triggerUpdate(this);
+    }
+
+    @Override
+    public void importDisabled() {
+        showSnackbar(R.string.local_update_import_disabled, Snackbar.LENGTH_LONG);
+    }
+
+    @Override
+    public void importFailed() {
+        showSnackbar(R.string.local_update_import_failure, Snackbar.LENGTH_LONG);
+    }
+
     private void hideUpdates() {
         findViewById(R.id.no_new_updates_view).setVisibility(View.VISIBLE);
         findViewById(R.id.recycler_view).setVisibility(View.GONE);
@@ -325,6 +346,14 @@ public class UpdatesActivity extends UpdatesListActivity {
         mExtrasFragment.updatePrefs();
         Log.d(TAG, "Adding remote updates");
         UpdaterController controller = mUpdaterService.getUpdaterController();
+
+        final Update currUpdate = controller.getCurrentUpdate();
+        if (currUpdate != null && currUpdate.getDownloadId().equals(Update.LOCAL_ID)) {
+            showUpdates();
+            mAdapter.setDownloadId(Update.LOCAL_ID);
+            mAdapter.notifyDataSetChanged();
+            return;
+        }
 
         UpdateInfo newUpdate = Utils.parseJson(jsonFile, true, this);
         boolean updateAvailable = newUpdate != null;
@@ -450,6 +479,9 @@ public class UpdatesActivity extends UpdatesListActivity {
     }
 
     private void handleStatusChange(UpdateStatus status) {
+        if (mUpdaterService.getUpdaterController().getCurrentUpdate().getDownloadId().equals(Update.LOCAL_ID)) {
+            return;
+        }
         if (mUpdateStatus == status){
             return;
         }

src/org/nameless/updates/UpdatesListAdapter.java
@@ -46,6 +46,7 @@ import org.nameless.updates.controller.UpdaterController;
 import org.nameless.updates.misc.Constants;
 import org.nameless.updates.misc.StringGenerator;
 import org.nameless.updates.misc.Utils;
+import org.nameless.updates.model.Update;
 import org.nameless.updates.model.UpdateInfo;
 import org.nameless.updates.model.UpdateStatus;
 
@@ -381,7 +382,8 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
 
         deleteAction.setVisible(canDelete);
         exportAction.setVisible(
-                Utils.getPersistentStatus(mContext) == UpdateStatus.Persistent.VERIFIED);
+                Utils.getPersistentStatus(mContext) == UpdateStatus.Persistent.VERIFIED &&
+                !mUpdate.getDownloadId().equals(Update.LOCAL_ID));
 
         popupMenu.setOnMenuItemClickListener(item -> {
             switch (item.getItemId()) {

ShadeDark6/android_packages_apps_Updates

src/org/zephyrus/ota/UpdatesActivity.java
@@ -57,6 +57,7 @@ import org.zephyrus.ota.controller.UpdaterService;
 import org.zephyrus.ota.download.DownloadClient;
 import org.zephyrus.ota.misc.Constants;
 import org.zephyrus.ota.misc.Utils;
+import org.zephyrus.ota.model.Update;
 import org.zephyrus.ota.model.UpdateInfo;
 import org.zephyrus.ota.model.UpdateStatus;
 
@@ -66,7 +67,7 @@ import java.util.UUID;
 
 import static org.zephyrus.ota.model.UpdateStatus.UNKNOWN;
 
-public class UpdatesActivity extends UpdatesListActivity {
+public class UpdatesActivity extends UpdatesListActivity implements UpdateListener {
 
     private static final String TAG = "UpdatesActivity";
 
@@ -308,6 +309,25 @@ public class UpdatesActivity extends UpdatesListActivity {
         return true;
     }
 
+    @Override
+    public void addedUpdate(Update update) {
+        UpdaterController controller = mUpdaterService.getUpdaterController();
+        Utils.setPersistentStatus(this, UpdateStatus.Persistent.VERIFIED);
+        controller.addUpdate(update);
+        getUpdatesList();
+        Utils.triggerUpdate(this);
+    }
+
+    @Override
+    public void importDisabled() {
+        showSnackbar(R.string.local_update_import_disabled, Snackbar.LENGTH_LONG);
+    }
+
+    @Override
+    public void importFailed() {
+        showSnackbar(R.string.local_update_import_failure, Snackbar.LENGTH_LONG);
+    }
+
     private void hideUpdates() {
         findViewById(R.id.no_new_updates_view).setVisibility(View.VISIBLE);
         findViewById(R.id.recycler_view).setVisibility(View.GONE);
@@ -326,6 +346,14 @@ public class UpdatesActivity extends UpdatesListActivity {
         mExtrasFragment.updatePrefs();
         Log.d(TAG, "Adding remote updates");
         UpdaterController controller = mUpdaterService.getUpdaterController();
+
+        final Update currUpdate = controller.getCurrentUpdate();
+        if (currUpdate != null && currUpdate.getDownloadId().equals(Update.LOCAL_ID)) {
+            showUpdates();
+            mAdapter.setDownloadId(Update.LOCAL_ID);
+            mAdapter.notifyDataSetChanged();
+            return;
+        }
 
         UpdateInfo newUpdate = Utils.parseJson(jsonFile, true, this);
         boolean updateAvailable = newUpdate != null;
@@ -451,6 +479,9 @@ public class UpdatesActivity extends UpdatesListActivity {
     }
 
     private void handleStatusChange(UpdateStatus status) {
+        if (mUpdaterService.getUpdaterController().getCurrentUpdate().getDownloadId().equals(Update.LOCAL_ID)) {
+            return;
+        }
         if (mUpdateStatus == status){
             return;
         }

src/org/zephyrus/ota/UpdatesListAdapter.java
@@ -46,6 +46,7 @@ import org.zephyrus.ota.controller.UpdaterController;
 import org.zephyrus.ota.misc.Constants;
 import org.zephyrus.ota.misc.StringGenerator;
 import org.zephyrus.ota.misc.Utils;
+import org.zephyrus.ota.model.Update;
 import org.zephyrus.ota.model.UpdateInfo;
 import org.zephyrus.ota.model.UpdateStatus;
 
@@ -381,7 +382,8 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
 
         deleteAction.setVisible(canDelete);
         exportAction.setVisible(
-                Utils.getPersistentStatus(mContext) == UpdateStatus.Persistent.VERIFIED);
+                Utils.getPersistentStatus(mContext) == UpdateStatus.Persistent.VERIFIED &&
+                !mUpdate.getDownloadId().equals(Update.LOCAL_ID));
 
         popupMenu.setOnMenuItemClickListener(item -> {
             switch (item.getItemId()) {